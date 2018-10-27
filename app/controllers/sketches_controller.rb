f318cb43ae1e462441dc13f34f7a9db05caebc16
mashineforkill <mashineforkill@gmail.com>
2018-10-27 20:07:20 +0300

    new changes

--- app/controllers/sketches_controller.rb
+++ app/controllers/sketches_controller.rb
@@ -4,30 +4,47 @@ class SketchesController < ApplicationController
     @sketches = Sketch.all
   end
 
-  def new
-    @sketch = Sketch.new
-  end
-
-  def edit
-  end
-
   def show
     @sketch = Sketch.find(params[:id])
   end
 
-  def create
-    # render plain: params[:sketch].inspect
-    @sketch = Sketch.new(sketch_params)
+  # def new
+  #   @sketch = Sketch.new
+  # end
+  #
+  # def edit
+  #   @sketch = Sketch.find(params[:id])
+  # end
+  #
+  # def create
+  #   # render plain: params[:sketch].inspect
+  #   @sketch = Sketch.new(sketch_params)
+  #
+  #   if(@sketch.save)
+  #     redirect_to @sketch
+  #   else
+  #     render 'new'
+  #   end
+  # end
 
-    if(@sketch.save)
-      redirect_to @sketch
-    else
-      render 'new'
-    end
+  # def update
+  #   @sketch = Sketch.find(params[:id])
+  #   if(@sketch.update(sketch_params))
+  #     redirect_to @sketch
+  #   else
+  #     render 'edit'
+  #   end
+  # end
+
+
+  def destroy
+    @sketch = Sketch.find(params[:id])
+    @sketch.destroy
+
+    redirect_to sketches_path
   end
 
-  private
-    def sketch_params
+  private def sketch_params
       params.require(:sketch).permit(:title,:text)
     end
-  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :sketches;
 
   root 'welcome#Index', as: 'home'
+  # root 'sketches#destroy', as: 'destroy_sketch'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
